924b4864c770a3ce4588a84d5f60310abd55bf02
Hedari <hedarikun@gmail.com>
2023-09-16 20:11:01 +0300

    add index point

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export * from "./components/alert.svelte"
+
+export {default as Button} from "./components/button.svelte"
+export * from "./components/buttonGroup.svelte"
+
+export * from "./components/input.svelte"
+export * from "./components/widget.svelte"
